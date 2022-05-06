61f19a044021b6d3f2040ee167f0985f5c4a6a04
Robert Sutherland <robert83sutherland@gmail.com>
2022-05-06 14:15:49 +0900

    setup: use react-p5 to embed sketches.

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,6 +1,8 @@
 * [x] Clear out mdx config.
-* [ ] Cleaner way to write processing code in react.
+* [x] Cleaner way to write processing code in react.
     - https://github.com/Gherciu/react-p5#readme
+* [ ] Create utility functions library
+    - grid-lines
 * [ ] Show graphs of x-y and x1 x2
 * [ ] Add sliders to play with variables.
 * [ ] Solve harmonic oscillator problems with calculus and linear analysis.
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,19 +1,46 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import TeX from "@matejmazur/react-katex";
-import { withPrefix } from "gatsby";
 import Layout from "../../components/layout";
+import p5 from "p5";
+import Sketch from "react-p5";
+
+const DampedHarmonicOscillator = () => {
+  const pointRadius = 5;
+  const [m, b, k] = [5, -0.01, 10];
+  let [x1, x2, t, dt] = [0, 100, 0, 0.1];
+
+  function setup(p: p5, canvasParentRef: Element) {
+    const canvas = p.createCanvas(400, 400);
+    canvas.style("border", "solid");
+  }
+
+  function draw(p: p5) {
+    p.translate(200, 200);
+    update();
+    p.ellipse(x1, x2, pointRadius, pointRadius);
+  }
+
+  function update() {
+    x1 += x2 * dt;
+    x2 += (-b / m) * x2 - (k / m) * x1 * dt;
+    t += dt;
+  }
+
+  return <Sketch setup={setup} draw={draw} />;
+};
 
 const Test = () => {
   return (
     <Layout>
       <Helmet>
         <title>Test</title>
-        <script src={withPrefix("sketches/pages/test/particleSystem.js")} />
       </Helmet>
-      <h1>Math from 1984</h1>
-      <p>The year is 1984</p>
-      <TeX math="2 + 2 = 5" block />
+      <h1>Damped Harmonic Oscillator</h1>
+      <p>The equation for the damped harmonic oscillator.</p>
+      <TeX math="m \dfrac{d^2x}{dt^2} + b \dfrac{dx}{dt} + kx = 0" block />
+      <DampedHarmonicOscillator />
+      <TeX math="test" block />
     </Layout>
   );
 };
